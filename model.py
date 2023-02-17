314caac1a10a4d7c9d2ab3b4483f6bfcaaf810db
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2023-02-17 22:42:55 +0530

    test

--- model.py
+++ model.py
@@ -178,7 +178,7 @@ class LitDiffusionModel(pl.LightningModule):
                 if return_intermediate:
                     ls.append(xt.cpu().detach().numpy())
         
-        print("1 ==> ",xt.cpu().detach().numpy().shape)
+        print("1 ==> ",xt.cpu().detach().numpy().shape," 2 => ",np.array(ls).shape)
         return xt.cpu().detach().numpy() if not return_intermediate else xt.cpu().detach().numpy(), np.array(ls)
 
     def configure_optimizers(self):
